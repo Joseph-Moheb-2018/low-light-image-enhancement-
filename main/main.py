0d1ba939f3a0fe70a8544c44f741b8f705c74cf6
MazenRefaat23 <36542052+MazenRefaat23@users.noreply.github.com>
2020-05-12 19:18:24 +0200

    Update main.py
    loading the trained model,
    implement the algorithm in the GUI

--- main/main.py
+++ main/main.py
@@ -13,6 +13,33 @@ import cv2
 import imageio
 from tensorflow.keras.models import load_model
 
+Model = load_model('model.h5')   #loading model
+
+'''we converted the steps of iterations into recursive function.
+inputs:
+img: the low light image (I, Eold)
+index: no of iterations
+flag: default = 1'''
+
+def Enhance(img, index, flag):
+    if index == 0:
+      return img
+
+    elif flag == 1:
+        h, w, c = img.shape
+        test = Model.predict(img.reshape(1, h, w, 3))
+        temp = img / 255
+        image = temp + ((test[0,:,:,:] * temp)*(1-temp))
+        index = index - 1
+        flag = 0
+        return Enhance(image, index, flag)
+
+    else:
+        h, w, c = img.shape
+        temp = Model.predict(img.reshape(1, h, w, 3))
+        image = img + ((temp[0,:,:,:] * img)*(1-img))
+        index = index - 1
+        return Enhance(image, index, flag)
 
 #loading path of GUI design
 FORM_CLASS, _ = loadUiType(path.join(path.dirname(__file__), "main2.ui"))
@@ -41,6 +68,17 @@ class Main(QWidget, FORM_CLASS):
 
     def Handel_Enhance(self):
         Input = imageio.imread(self.name)   #Reading the input image
+        
+        mean = np.mean(Input)               #get mean of image to know the degree of darkness
+        
+        if mean < 10:  #specify the number of iterations suitable for the input image
+            index = 12  #12 if extreme low light
+        else:
+            index = 8   #8 if medium low light
+        
+        out = Enhance(Input, index, 1)         #apply model to input image
+             
+        output = (out * 255).astype("uint8")   #convert range of output from [0:1] to [0:255]
         
         self.image = output                    #have variable holding the output image
 
